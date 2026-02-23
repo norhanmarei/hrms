e1751d324815bfa54f6f31aa71977f4f7d36d1b4
Norhan Marei <mustafanour751@gmail.com>
2026-02-23 20:29:33 +0300

    feat: add DTOs and enum to help job titles service

---
+++ src/HRMS.Application/Enums/JobTitleActionResult.cs
@@ -0,0 +1,12 @@
+namespace HRMS.Application.Enums
+{
+    public enum JobTitleActionResult
+    {
+        NotFound = 0,
+        NameAlreadyExists = 1,
+        Success = 2,
+        Failure = 3,
+        AlreadyActive = 4,
+        AlreadyNotActive = 5
+    }
+}
