723ecd039d2e1309b03cd10a2fdba9b7fa80580e
Norhan Marei <mustafanour751@gmail.com>
2026-02-23 20:30:04 +0300

    refactor: modified job title entity

--- src/HRMS.Domain/Entities/JobTitle.cs
+++ src/HRMS.Domain/Entities/JobTitle.cs
@@ -3,12 +3,21 @@ using HRMS.Domain.Common;
 
 namespace HRMS.Domain.Entities
 {
-    public class JobTitle : AuditableEntity
+    public class JobTitle
     {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; } 
-        public int DepartmentId { get; set; }
-        public Department? Department { get; set; }
+        public int Id{ get; set; }
+        public string Name { get; set; } 
+        public string? Description { get; set; }
         public bool IsActive { get; set; } = true;
+        public JobTitle() { }
+        public JobTitle(string name, string? description)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                Name = name;
+                IsActive = true;
+            }
+            if(!string.IsNullOrWhiteSpace(description)) Description = description;
+        }
     }
 }
